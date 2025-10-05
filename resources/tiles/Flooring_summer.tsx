6a074355f2d3129814e799a84e26f0e8ae9b12bf
AchiZ48 <cemefe11@hotmail.com>
2025-10-06 03:27:30 +0700

    path sprite

---
+++ resources/tiles/Flooring_summer.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Flooring_summer" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Flooring_summer.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="path" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="193" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="194" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="195" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="209" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="210" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="211" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="225" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="226" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="227" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="193" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="194" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="195" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="209" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="211" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="225" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="226" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="227" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
