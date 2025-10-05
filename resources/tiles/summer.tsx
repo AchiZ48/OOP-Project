1d441641b1db2b921e552ad8dd8c276e9783662d
AchiZ48 <cemefe11@hotmail.com>
2025-10-06 03:36:30 +0700

    edit map

--- resources/tiles/summer.tsx
+++ resources/tiles/summer.tsx
@@ -1,4 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="summer.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="dirt" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="g" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="3" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="33" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="34" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="35" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="36" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="48" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="49" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="50" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="51" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="52" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="64" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="65" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="66" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="67" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="68" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="80" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="81" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="82" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="83" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="84" wangid="0,1,0,2,0,2,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
