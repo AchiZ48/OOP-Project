b87c15019a2b022d8332b5847426a6dedc539583
AchiZ48 <cemefe11@hotmail.com>
2025-10-09 21:10:08 +0700

    update

---
+++ resources/tiles/deeproot.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="deeproot" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="deeproot.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="tile" type="corner" tile="-1">
+   <wangcolor name="dirt" color="#ff0000" tile="17" probability="1"/>
+   <wangcolor name="water" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="35" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="37" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="48" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="50" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="100" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="101" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="116" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="117" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="128" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="129" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="130" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="131" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="132" wangid="0,2,0,0,0,2,0,0"/>
+   <wangtile tileid="133" wangid="0,0,0,2,0,0,0,2"/>
+   <wangtile tileid="144" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="145" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="147" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="160" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="163" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="176" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="177" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="178" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="179" wangid="0,0,0,0,0,0,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
