7a4d9aa15f8739e72f0a9b7c1243e629706ad616
AchiZ48 <cemefe11@hotmail.com>
2025-10-11 05:30:08 +0700

    update map

--- resources/tiles/summer.tsx
+++ resources/tiles/summer.tsx
@@ -1,8 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="summer.png" width="512" height="512"/>
+ <tile id="16" probability="0.122"/>
+ <tile id="32" probability="0.122"/>
+ <tile id="48" probability="0.122"/>
+ <tile id="64" probability="0.122"/>
+ <tile id="80" probability="0.122"/>
+ <tile id="96" probability="0.018"/>
+ <tile id="97" probability="0.018"/>
+ <tile id="98" probability="0.018"/>
+ <tile id="99" probability="0.018"/>
+ <tile id="100" probability="0.003"/>
+ <tile id="109" probability="0.015"/>
+ <tile id="112" probability="0.018"/>
+ <tile id="113" probability="0.018"/>
+ <tile id="114" probability="0.018"/>
+ <tile id="115" probability="0.018"/>
+ <tile id="116" probability="0.003"/>
+ <tile id="128" probability="0.03"/>
+ <tile id="132" probability="0.003"/>
+ <tile id="144" probability="0.03"/>
+ <tile id="145" probability="0.03"/>
+ <tile id="146" probability="0.03"/>
+ <tile id="148" probability="0.003"/>
  <wangsets>
-  <wangset name="dirt" type="corner" tile="-1">
+  <wangset name="dirt" type="corner" tile="98">
    <wangcolor name="dirt" color="#ff0000" tile="52" probability="1"/>
    <wangcolor name="grass" color="#00ff00" tile="16" probability="1"/>
    <wangcolor name="water" color="#0000ff" tile="220" probability="1"/>
--- resources/tiles/winter.tsx
+++ resources/tiles/winter.tsx
@@ -1,12 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="winter" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="winter.png" width="512" height="512"/>
- <tile id="100" probability="0.1"/>
- <tile id="109" probability="0.1"/>
- <tile id="116" probability="0.1"/>
- <tile id="128" probability="0.1"/>
- <tile id="132" probability="0.1"/>
- <tile id="144" probability="0.1"/>
- <tile id="145" probability="0.1"/>
+ <tile id="16" probability="0.122"/>
+ <tile id="32" probability="0.122"/>
+ <tile id="48" probability="0.122"/>
+ <tile id="64" probability="0.122"/>
+ <tile id="80" probability="0.122"/>
+ <tile id="96" probability="0.018"/>
+ <tile id="97" probability="0.018"/>
+ <tile id="98" probability="0.018"/>
+ <tile id="99" probability="0.018"/>
+ <tile id="100" probability="0.003"/>
+ <tile id="109" probability="0.015"/>
+ <tile id="112" probability="0.018"/>
+ <tile id="113" probability="0.018"/>
+ <tile id="114" probability="0.018"/>
+ <tile id="115" probability="0.018"/>
+ <tile id="116" probability="0.003"/>
+ <tile id="128" probability="0.03"/>
+ <tile id="132" probability="0.003"/>
+ <tile id="144" probability="0.03"/>
+ <tile id="145" probability="0.03"/>
+ <tile id="146" probability="0.03"/>
  <tile id="148" probability="0.1"/>
 </tileset>
